317cfb9601b3d34be61da9a5b57d5b4b7860ab05
Tiffany <tcede001@ucr.edu>
2017-11-27 18:28:46 -0800

    added data set files

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+	//output message
+	string filename; 
+	cout << "Welcome to Tiffany Cedeno's Feature Selection Algorithm.\n"; 
+	cout << "Type in the name of the file to test: "; 
+	cin >> filename; 
+	return 0; 
+}
